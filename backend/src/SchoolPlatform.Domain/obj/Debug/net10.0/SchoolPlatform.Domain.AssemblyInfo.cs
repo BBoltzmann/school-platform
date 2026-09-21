040b2d2c73330332eb7953b6c7fa23416195df73
BBoltzmann <woleoshoifeoluwa@gmail.com>
2026-09-21 12:41:06 +0100

    Finish timetable scheduling and printable exports

--- backend/src/SchoolPlatform.Domain/obj/Debug/net10.0/SchoolPlatform.Domain.AssemblyInfo.cs
+++ backend/src/SchoolPlatform.Domain/obj/Debug/net10.0/SchoolPlatform.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolPlatform.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf0d049788de007cbc0a4caae1f7f88147e6c99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c74d15d143aa8734c8c201453fe3fee3577ac15b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolPlatform.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolPlatform.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
